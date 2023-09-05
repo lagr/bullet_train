f307d0dbc153d75449c16ec953cb1f8cf826ccbb
Kasper Timm Hansen <kaspth@gmail.com>
2023-09-05 22:04:12 +0200

    Run `bin/update/system_tests/use_device_test` in Bullet Train
    Follow up to https://github.com/bullet-train-co/bullet_train/pull/720

--- test/system/application_platform_test.rb
+++ test/system/application_platform_test.rb
@@ -14,68 +14,64 @@ class ApplicationPlatformSystemTest < ApplicationSystemTestCase
     end
   end
 
-  @@test_devices.each do |device_name, display_details|
-    test "visitors can sign-up and manage team members with subscriptions #{billing_enabled? ? "enabled" : "disabled"} on a #{device_name}" do
-      resize_for(display_details)
+  device_test "visitors can sign-up and manage team members with subscriptions #{billing_enabled? ? "enabled" : "disabled"}" do
+    be_invited_to_sign_up
 
-      be_invited_to_sign_up
+    visit root_path
+    sign_up_from_homepage_for(display_details)
 
-      visit root_path
-      sign_up_from_homepage_for(display_details)
+    # try non-matching passwords.
+    fill_in "Your Email Address", with: "jane.smith@gmail.com"
+    fill_in "Set Password", with: example_password
+    fill_in "Confirm Password", with: example_password
+    click_on "Sign Up"
 
-      # try non-matching passwords.
-      fill_in "Your Email Address", with: "jane.smith@gmail.com"
-      fill_in "Set Password", with: example_password
-      fill_in "Confirm Password", with: example_password
-      click_on "Sign Up"
-
-      if billing_enabled?
-        complete_pricing_page
-      end
-
-      # we should now be on an onboarding step.
-      assert page.has_content?("Tell us about you")
-      fill_in "First Name", with: "Jane"
-      fill_in "Last Name", with: "Smith"
-      fill_in "Your Team Name", with: "The Testing Team"
-      click_on "Next"
-
-      # Create a new Platform Application
-      within_developers_menu_for(display_details) do
-        click_on "API"
-      end
-      click_on "Provision New Platform Application"
-      fill_in "Name", with: "Test Platform Application"
-      click_on "Provision Platform Application"
-      assert page.has_content?("Platform Application was successfully created.")
-
-      @team = Team.find_by(name: "The Testing Team")
-
-      # Ensure that Platform Application is present in the Memberships list.
-      visit account_team_memberships_path(@team)
-      within_current_memberships_table do
-        assert page.has_content?("Test Platform Application")
-      end
-
-      # Remove the Platform Application and ensure it's
-      # not present in the tombstoned Memberships list.
-      within_developers_menu_for(display_details) do
-        click_on "API"
-      end
-      accept_alert { click_on("Delete") }
-
-      visit account_team_memberships_path(@team)
-      within_current_memberships_table do
-        refute page.has_content?("Test Platform Application")
-      end
-      # The tombstones partial won't be rendered if there aren't any tombstoned memberships.
-      # TODO: This eats up a lot of time, look for a better way to check for this.
-      refute page.has_css?("tbody[data-model='Membership'][data-scope='tombstones']")
-
-      # The Membership was archived but not destroyed.
-      test_app_membership = Membership.find_by(user_first_name: "Test Platform Application")
-      assert test_app_membership.present?
-      refute test_app_membership.tombstone?
+    if billing_enabled?
+      complete_pricing_page
     end
+
+    # we should now be on an onboarding step.
+    assert page.has_content?("Tell us about you")
+    fill_in "First Name", with: "Jane"
+    fill_in "Last Name", with: "Smith"
+    fill_in "Your Team Name", with: "The Testing Team"
+    click_on "Next"
+
+    # Create a new Platform Application
+    within_developers_menu_for(display_details) do
+      click_on "API"
+    end
+    click_on "Provision New Platform Application"
+    fill_in "Name", with: "Test Platform Application"
+    click_on "Provision Platform Application"
+    assert page.has_content?("Platform Application was successfully created.")
+
+    @team = Team.find_by(name: "The Testing Team")
+
+    # Ensure that Platform Application is present in the Memberships list.
+    visit account_team_memberships_path(@team)
+    within_current_memberships_table do
+      assert page.has_content?("Test Platform Application")
+    end
+
+    # Remove the Platform Application and ensure it's
+    # not present in the tombstoned Memberships list.
+    within_developers_menu_for(display_details) do
+      click_on "API"
+    end
+    accept_alert { click_on("Delete") }
+
+    visit account_team_memberships_path(@team)
+    within_current_memberships_table do
+      refute page.has_content?("Test Platform Application")
+    end
+    # The tombstones partial won't be rendered if there aren't any tombstoned memberships.
+    # TODO: This eats up a lot of time, look for a better way to check for this.
+    refute page.has_css?("tbody[data-model='Membership'][data-scope='tombstones']")
+
+    # The Membership was archived but not destroyed.
+    test_app_membership = Membership.find_by(user_first_name: "Test Platform Application")
+    assert test_app_membership.present?
+    refute test_app_membership.tombstone?
   end
 end
--- test/system/membership_test.rb
+++ test/system/membership_test.rb
@@ -19,137 +19,133 @@ class MembershipSystemTest < ApplicationSystemTestCase
     end
   end
 
-  @@test_devices.each do |device_name, display_details|
-    test "visitors can sign-up and manage team members with subscriptions #{billing_enabled? ? "enabled" : "disabled"} on a #{device_name}" do
-      resize_for(display_details)
+  device_test "visitors can sign-up and manage team members with subscriptions #{billing_enabled? ? "enabled" : "disabled"}" do
+    be_invited_to_sign_up
 
-      be_invited_to_sign_up
+    visit root_path
+    sign_up_from_homepage_for(display_details)
 
-      visit root_path
-      sign_up_from_homepage_for(display_details)
+    # try non-matching passwords.
+    fill_in "Your Email Address", with: "jane.smith@gmail.com"
+    fill_in "Set Password", with: example_password
+    fill_in "Confirm Password", with: example_password
+    click_on "Sign Up"
 
-      # try non-matching passwords.
-      fill_in "Your Email Address", with: "jane.smith@gmail.com"
-      fill_in "Set Password", with: example_password
-      fill_in "Confirm Password", with: example_password
-      click_on "Sign Up"
+    if billing_enabled?
+      complete_pricing_page
+    end
 
-      if billing_enabled?
-        complete_pricing_page
+    # we should now be on an onboarding step.
+    assert page.has_content?("Tell us about you")
+    fill_in "First Name", with: "Jane"
+    fill_in "Last Name", with: "Smith"
+    fill_in "Your Team Name", with: "The Testing Team"
+    click_on "Next"
+
+    within_team_menu_for(display_details) do
+      click_on "Team Members"
+    end
+
+    assert page.has_content?("The Testing Team Team Members")
+    click_on "Invite a New Team Member"
+
+    fill_in "Email Address", with: "takashi.yamaguchi@gmail.com"
+    fill_in "First Name", with: "Takashi"
+    fill_in "Last Name", with: "Yamaguchi"
+    find("label", text: "Invite as Team Administrator").click
+    click_on "Create Invitation"
+
+    assert page.has_content?("Invitation was successfully created.")
+
+    # we need the id of the membership that's created so we can address it's row in the table specifically.
+    invited_membership = Membership.order(:id).last
+
+    within_current_memberships_table do
+      assert page.has_content?("Takashi Yamaguchi")
+      within_membership_row(invited_membership) do
+        assert page.has_content?("Invited")
+        assert page.has_content?("Team Administrator")
+        click_on "Details"
       end
+    end
 
-      # we should now be on an onboarding step.
-      assert page.has_content?("Tell us about you")
-      fill_in "First Name", with: "Jane"
-      fill_in "Last Name", with: "Smith"
-      fill_in "Your Team Name", with: "The Testing Team"
-      click_on "Next"
+    assert page.has_content?("SPECIAL PRIVILEGES")
+    assert page.has_content?("Team Administrator")
 
-      within_team_menu_for(display_details) do
-        click_on "Team Members"
+    accept_alert do
+      click_on "Demote from Admin"
+    end
+
+    within_current_memberships_table do
+      within_membership_row(invited_membership) do
+        assert page.has_no_content?("Team Administrator")
+        assert page.has_content?("Viewer")
+        click_on "Details"
       end
+    end
 
-      assert page.has_content?("The Testing Team Team Members")
-      click_on "Invite a New Team Member"
+    assert page.has_content?("SPECIAL PRIVILEGES")
+    assert page.has_no_content?("Team Administrator")
+    assert page.has_content?("Viewer")
 
-      fill_in "Email Address", with: "takashi.yamaguchi@gmail.com"
-      fill_in "First Name", with: "Takashi"
-      fill_in "Last Name", with: "Yamaguchi"
-      find("label", text: "Invite as Team Administrator").click
-      click_on "Create Invitation"
+    accept_alert { click_on "Promote to Admin" }
 
-      assert page.has_content?("Invitation was successfully created.")
-
-      # we need the id of the membership that's created so we can address it's row in the table specifically.
-      invited_membership = Membership.order(:id).last
-
-      within_current_memberships_table do
-        assert page.has_content?("Takashi Yamaguchi")
-        within_membership_row(invited_membership) do
-          assert page.has_content?("Invited")
-          assert page.has_content?("Team Administrator")
-          click_on "Details"
-        end
+    within_current_memberships_table do
+      within_membership_row(invited_membership) do
+        assert page.has_content?("Team Administrator")
+        click_on "Details"
       end
+    end
 
-      assert page.has_content?("SPECIAL PRIVILEGES")
-      assert page.has_content?("Team Administrator")
+    assert page.has_content? "Invitation Details"
+    click_on "Settings"
 
-      accept_alert do
-        click_on "Demote from Admin"
-      end
+    assert page.has_content? "Update Membership Settings"
+    fill_in "First Name", with: "Yuto"
+    fill_in "Last Name", with: "Nishiyama"
+    # this is removing their admin privileges.
+    find("label", text: "Grant Privileges of Team Administrator").click
+    click_on "Update Membership"
 
-      within_current_memberships_table do
-        within_membership_row(invited_membership) do
-          assert page.has_no_content?("Team Administrator")
-          assert page.has_content?("Viewer")
-          click_on "Details"
-        end
-      end
+    assert page.has_no_content?("Takashi Yamaguchi")
+    assert page.has_no_content?("Team Administrator")
 
-      assert page.has_content?("SPECIAL PRIVILEGES")
-      assert page.has_no_content?("Team Administrator")
-      assert page.has_content?("Viewer")
+    assert page.has_content?("Yuto Nishiyama")
+    assert page.has_content?("Viewer")
 
-      accept_alert { click_on "Promote to Admin" }
+    accept_alert { click_on "Remove from Team" }
 
-      within_current_memberships_table do
-        within_membership_row(invited_membership) do
-          assert page.has_content?("Team Administrator")
-          click_on "Details"
-        end
-      end
+    within_current_memberships_table do
+      assert page.has_no_content?("Yuto Nishiyama")
+    end
 
-      assert page.has_content? "Invitation Details"
-      click_on "Settings"
+    # We assume all of the membership information is still there, even if the user never registered.
+    unregistered_membership = Membership.find_by(user_first_name: "Yuto", user_last_name: "Nishiyama")
+    assert unregistered_membership.user_id.nil?
+    assert unregistered_membership.invitation.nil?
+    assert unregistered_membership.tombstone?
+    assert unregistered_membership.user_email.present?
 
-      assert page.has_content? "Update Membership Settings"
-      fill_in "First Name", with: "Yuto"
-      fill_in "Last Name", with: "Nishiyama"
-      # this is removing their admin privileges.
-      find("label", text: "Grant Privileges of Team Administrator").click
-      click_on "Update Membership"
-
-      assert page.has_no_content?("Takashi Yamaguchi")
-      assert page.has_no_content?("Team Administrator")
-
+    within_former_memberships_table do
       assert page.has_content?("Yuto Nishiyama")
       assert page.has_content?("Viewer")
-
-      accept_alert { click_on "Remove from Team" }
-
-      within_current_memberships_table do
-        assert page.has_no_content?("Yuto Nishiyama")
-      end
-
-      # We assume all of the membership information is still there, even if the user never registered.
-      unregistered_membership = Membership.find_by(user_first_name: "Yuto", user_last_name: "Nishiyama")
-      assert unregistered_membership.user_id.nil?
-      assert unregistered_membership.invitation.nil?
-      assert unregistered_membership.tombstone?
-      assert unregistered_membership.user_email.present?
-
-      within_former_memberships_table do
-        assert page.has_content?("Yuto Nishiyama")
-        assert page.has_content?("Viewer")
-        accept_alert { click_on "Re-Invite to Team" }
-      end
-
-      assert page.has_content?("The user has been successfully re-invited. They will receive an email to rejoin the team.")
-
-      within_current_memberships_table do
-        assert page.has_content?("Yuto Nishiyama")
-        within_membership_row(invited_membership) do
-          assert page.has_content?("Viewer")
-          click_on "Details"
-        end
-      end
-
-      assert page.has_content?("Yuto Nishiyama’s Membership on The Testing Team")
-      assert page.has_content?("Invitation Details")
-      click_on "Back"
-
-      assert page.has_content?("The Testing Team Team Members")
+      accept_alert { click_on "Re-Invite to Team" }
     end
+
+    assert page.has_content?("The user has been successfully re-invited. They will receive an email to rejoin the team.")
+
+    within_current_memberships_table do
+      assert page.has_content?("Yuto Nishiyama")
+      within_membership_row(invited_membership) do
+        assert page.has_content?("Viewer")
+        click_on "Details"
+      end
+    end
+
+    assert page.has_content?("Yuto Nishiyama’s Membership on The Testing Team")
+    assert page.has_content?("Invitation Details")
+    click_on "Back"
+
+    assert page.has_content?("The Testing Team Team Members")
   end
 end
